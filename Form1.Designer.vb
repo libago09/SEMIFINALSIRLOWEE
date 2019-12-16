ae1731623dfd19eef0ba06fe8afde88c1e400558
libago09 <48169985+libago09@users.noreply.github.com>
2019-12-17 00:45:47 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Storm1 = New System.Windows.Forms.PictureBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Storm2 = New System.Windows.Forms.PictureBox()
+        Me.Storm3 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Storm1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Storm2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Storm3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Green
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(760, 540)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 50
+        '
+        'Storm1
+        '
+        Me.Storm1.BackColor = System.Drawing.Color.Green
+        Me.Storm1.Image = Global.SNAKEge.My.Resources.Resources._31323_7_storm_free_download
+        Me.Storm1.Location = New System.Drawing.Point(51, 524)
+        Me.Storm1.Name = "Storm1"
+        Me.Storm1.Size = New System.Drawing.Size(82, 94)
+        Me.Storm1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Storm1.TabIndex = 1
+        Me.Storm1.TabStop = False
+        Me.Storm1.Visible = False
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 250
+        '
+        'Storm2
+        '
+        Me.Storm2.BackColor = System.Drawing.Color.Green
+        Me.Storm2.Image = Global.SNAKEge.My.Resources.Resources._31323_7_storm_free_download
+        Me.Storm2.Location = New System.Drawing.Point(757, 34)
+        Me.Storm2.Name = "Storm2"
+        Me.Storm2.Size = New System.Drawing.Size(82, 94)
+        Me.Storm2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Storm2.TabIndex = 2
+        Me.Storm2.TabStop = False
+        Me.Storm2.Visible = False
+        '
+        'Storm3
+        '
+        Me.Storm3.BackColor = System.Drawing.Color.Green
+        Me.Storm3.Image = Global.SNAKEge.My.Resources.Resources._31323_7_storm_free_download
+        Me.Storm3.Location = New System.Drawing.Point(728, 505)
+        Me.Storm3.Name = "Storm3"
+        Me.Storm3.Size = New System.Drawing.Size(82, 94)
+        Me.Storm3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Storm3.TabIndex = 3
+        Me.Storm3.TabStop = False
+        Me.Storm3.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(784, 561)
+        Me.Controls.Add(Me.Storm3)
+        Me.Controls.Add(Me.Storm2)
+        Me.Controls.Add(Me.Storm1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Snake Game Daw"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Storm1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Storm2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Storm3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Storm1 As PictureBox
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Storm2 As PictureBox
+    Friend WithEvents Storm3 As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,180 @@
+﻿Public Class Form1
+
+    Dim snake(1000) As PictureBox
+    Dim ctrArray As Integer = -1
+    Dim lastKeyPressed As String = ""
+    Dim imageLoc1 As String = "C:\Users\Ryzen 5\Desktop\circle-png-15279.png"
+    Dim imageLoc2 As String = "C:\Users\Ryzen 5\Desktop\circle-png-15287.png"
+    Dim imageLoc3 As String = "C:\Users\Ryzen 5\Desktop\60-606864_apple-png.png"
+    Dim food As New PictureBox
+
+    Private Sub snakeHead()
+        ctrArray += 1
+
+        snake(ctrArray) = New PictureBox
+        With snake(ctrArray)
+            .Height = 20
+            .Width = 20
+            .BackColor = PictureBox1.BackColor
+            .Image = Image.FromFile(imageLoc1)
+            .SizeMode = PictureBoxSizeMode.StretchImage
+            .Top = (PictureBox1.Bottom + PictureBox1.Top) / 2
+            .Left = (PictureBox1.Left + PictureBox1.Right) / 2
+        End With
+        Me.Controls.Add(snake(ctrArray))
+        snake(ctrArray).BringToFront()
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        snakeHead()
+        createFood()
+        Timer1.Start()
+
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+
+        If ctrArray = 0 Then
+
+            If e.KeyCode = Keys.W Then     'can go a to d or s to w vice versa in ( without tail )
+                lastKeyPressed = "W"
+            End If
+            If e.KeyCode = Keys.A Then
+                lastKeyPressed = "A"
+            End If
+            If e.KeyCode = Keys.S Then
+                lastKeyPressed = "S"
+            End If
+            If e.KeyCode = Keys.D Then
+                lastKeyPressed = "D"
+            End If
+        Else
+            If e.KeyCode = Keys.W And String.Equals(lastKeyPressed, "S") = False Then
+                lastKeyPressed = "W"
+            End If
+            If e.KeyCode = Keys.A And String.Equals(lastKeyPressed, "D") = False Then
+                lastKeyPressed = "A"
+            End If
+            If e.KeyCode = Keys.S And String.Equals(lastKeyPressed, "W") = False Then
+                lastKeyPressed = "S"
+            End If
+            If e.KeyCode = Keys.D And String.Equals(lastKeyPressed, "A") = False Then
+                lastKeyPressed = "D"
+            End If
+
+        End If
+
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        checkWalls()
+        snakeEatsFood()
+        moveTail()
+        Select Case lastKeyPressed
+            Case "W"
+                snake(0).Top -= 20
+            Case "A"
+                snake(0).Left -= 20
+            Case "S"
+                snake(0).Top += 20
+            Case "D"
+                snake(0).Left += 20
+        End Select
+
+        checkIfBitesItself()
+        If ctrArray = 5 Then
+            Storm1.Visible = True
+            Storm2.Visible = True
+            Storm3.Visible = True
+            Timer2.Start()
+
+        End If
+    End Sub
+
+    Private Sub addTail()
+        ctrArray = ctrArray + 1
+        snake(ctrArray) = New PictureBox
+        With snake(ctrArray)
+            .Height = 20
+            .Width = 20
+            .BackColor = Color.White
+            '   .Image = Image.FromFile(imageLoc2)
+            .SizeMode = PictureBoxSizeMode.StretchImage
+        End With
+        Me.Controls.Add(snake(ctrArray))
+        snake(ctrArray).BringToFront()
+    End Sub
+
+    Private Sub moveTail()
+        Dim i As Integer
+
+        For i = ctrArray To 1 Step -1
+            snake(i).Top = snake(i - 1).Top
+            snake(i).Left = snake(i - 1).Left
+        Next
+    End Sub
+
+    Private Sub checkWalls()
+        If snake(0).Left < PictureBox1.Left Then
+            Timer1.Stop()
+        End If
+        If snake(0).Right > PictureBox1.Right Then
+            Timer1.Stop()
+        End If
+        If snake(0).Top < PictureBox1.Top Then
+            Timer1.Stop()
+        End If
+        If snake(0).Bottom > PictureBox1.Bottom Then
+            Timer1.Stop()
+        End If
+
+    End Sub
+
+    Private Sub createFood()
+
+        Dim rdn As New Random
+        With food
+            .Width = 30
+            .Height = 30
+            .Top = rdn.Next(40, PictureBox1.Bottom)
+            .Left = rdn.Next(40, PictureBox1.Right)
+            .BackColor = Color.Green
+            .Image = Image.FromFile(imageLoc3)
+            .SizeMode = PictureBoxSizeMode.StretchImage
+        End With
+
+        Me.Controls.Add(food)
+        food.BringToFront()
+    End Sub
+
+    Private Sub snakeEatsFood()
+        Dim rdn As New Random
+        If snake(0).Bounds.IntersectsWith(food.Bounds) Then
+            food.Top = rdn.Next(60, PictureBox1.Bottom - 60)
+            food.Left = rdn.Next(60, PictureBox1.Right - 60)
+            addTail()
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
+
+        Storm1.Top -= 10
+            Storm2.Left -= 10
+        Storm3.Left -= (snake(0).Left - 10)
+        Storm3.Top -= (snake(0).Top - 10)
+    End Sub
+
+    Private Sub checkIfBitesItself()
+        Dim x As Integer
+        For x = 1 To ctrArray
+            If snake(0).Bounds.IntersectsWith(snake(x).Bounds) Then
+                Timer1.Stop()
+                Timer2.Stop()
+                MsgBox("You bit your own tail! GGWP!!")
+            End If
+        Next
+    End Sub
+End Class
